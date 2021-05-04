b43b8123140129da9d50e046a9db1f8ae12d31c2
Nicolo Ruggeri <nicolo.ruggeri@tuebingen.mpg.de>
2021-05-04 13:27:41 +0200

    setup.py for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="tcec_sampling",
+    version="1.0",
+    packages=["tcec_sampling"],
+    url="https://github.com/cdebacco/tcec_sampling",
+    license="MIT",
+    author="Nicolò Ruggeri, Caterina De Bacco",
+    author_email="nicolo.ruggeri@tuebingen.mpg.de, caterina.debacco@tuebingen.mpg.de",
+    description="TCEC sampling algorithm on networks.",
+    install_requires=["numpy==1.18.5", "networkx==2.4"],
+)
